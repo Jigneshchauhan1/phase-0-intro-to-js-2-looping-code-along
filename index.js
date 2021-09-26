fd6605fa3323e322883ebd3f33ae455e1e8e85c3
Jignesh <jigneshmailbag@gmail.com>
2021-09-26 13:00:08 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(names, messages){
+    let myArr = [];
+    for (let i = 0; i < names.length; i++) {
+    let greetings = (`Thank you, ${names[i]}, for the wonderful ${messages} gift!`);
+     myArr.push(greetings);
+    }
+    return myArr;
+    
+}
+
+function countDown(integer){
+    while (integer >= 0) {
+        console.log(integer--);
+    }
+}
+
